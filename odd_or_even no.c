3e2c150061b5c7449061cf729fc4ab82358fcd4b
Deepa joshi <100574613+Dipajoshi013@users.noreply.github.com>
2022-03-16 23:05:45 +0545

    Create odd_or_even no.c

---
+++ odd_or_even no.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int no;
+    printf("enter a number");
+    scanf("%d",&no);
+    if(no%2==0)
+    printf("%d iseven",no);
+    else
+    printf("%d numberis odd",no);
+    return 0;
+}
