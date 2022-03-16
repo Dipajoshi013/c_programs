8228fb9338aaf1efa4171112eb523fe18cb40fdf
Deepa joshi <100574613+Dipajoshi013@users.noreply.github.com>
2022-03-16 22:22:49 +0545

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,sum,m;
+    system("cls");
+    printf("Enter any two numbers");
+    scanf("%d%d",&n,&m);
+    sum=m+n;
+    printf("The sum of two numbers is %d",sum);
+
+}
